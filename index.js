bf66ee85dbaed45ff066aadf10aae019999ab77e
Olanrewaju Gabriel Ibironke <brnkgabriel@gmail.com>
2022-07-10 07:10:50 +0100

    climbing leaderboard solution = started

--- index.js
+++ index.js
@@ -80,4 +80,83 @@ function divisibleSumPairs(n, k, ar) {
   return pairs.length
 }
 
-console.log(divisibleSumPairs(6, 3, [1, 3, 2, 6, 1, 2]))
+// console.log(divisibleSumPairs(6, 3, [1, 3, 2, 6, 1, 2]))
+
+function climbingLeaderboard2(ranked, player) {
+  // Write your code here
+  let posMap = {}
+  const posList = []
+  const scoresToPos = scores => {
+    const set = [...new Set(scores)].sort((a, b) => b - a)
+    set.map((score, idx) => posMap[score] = idx + 1)
+    return posMap
+  }
+  const start = Date.now()
+  while (player.length > 0) {
+    const currentScore = player.shift()
+    const temp = [...ranked, currentScore]
+    scoresToPos(temp)
+    posList.push(posMap[currentScore])
+    posMap = {}
+  }
+  console.log(Date.now() - start)
+  return posList
+}
+
+function climbingLeaderboard(ranked, player) {
+  const positions = []
+  const set = [...new Set(ranked)]
+  const start = Date.now()
+  for (let score of player) {
+    const pos = explorePosition(set, set.length + 1, score)
+    positions.push(pos)
+  }
+  console.log("TimeLapse", Date.now() - start)
+  return positions
+} 
+
+const explorePosition = (set, position, score, memo={}) => {
+  for (score in memo) return memo[score]
+  if (position === 0) return 1
+  if (score < set[position - 1]) return position + 1
+  --position
+  memo[score] = explorePosition(set, position, score)
+  return memo[score]
+}
+
+function climbingLeaderboard3(ranked, player) {
+  const set = [...new Set(ranked)]
+  const map = {}
+  set.forEach((num, idx) => map[num] = idx + 1)
+  let low = 0
+  let high = ranked.length - 1
+  let item = player[2]
+  const positions = []
+  // todo use binary search
+  while (low <= high) {
+    console.log(ranked, [low, high])
+    const mid = Math.floor((low + high) / 2)
+    const guess = ranked[mid]
+
+    if (guess === item) {
+      return mid
+    }
+
+    if (guess > item) {
+      high = mid - 1
+    } else {
+      low = mid + 1
+    }
+
+  }
+
+  return map
+}
+
+console.log(climbingLeaderboard3([100,90,90,80], [70,80,90,105]))
+// console.log(climbingLeaderboard3([100,100,50,40,40,20,10], [5,25,50,120]))
+// console.log(climbingLeaderboard3([100,90,90,80,75,60], [50,65,77,90,102]))
+
+// ranking = ranking.split(" ")
+// player  = player.split(" ")
+// console.log(climbingLeaderboard2(ranking, player))
